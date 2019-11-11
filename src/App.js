93a00fb92e12f29263ef08bf4abede81d4e93efe
DaltonEsc <eschberger.dalton@gmail.com>
2019-11-11 13:48:33 -0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+  render(){
+    return (
+       <div className="mainWrapper">
+        <div className="basicInfo">
+          <div className = "picture">
+            <p>Place Holder for Pic</p>
+          </div>
+          <div className = "contactInfo">
+            <p>Place holder for contactInfo</p>
+          </div>
+          <div className = "skills">
+            <p>Placeholder for skills</p>
+          </div>
+        </div>
+        <div className="experience">
+        <div className = "projects">
+          <p>placeholder for projects</p>
 
+        </div>
+        <div className = "school">
+          <p>placeholder for school info</p>
+
+        </div>
+        <div className = "work">
+          <p>placeholder for work exp</p>
+
+        </div>
+        </div>
+       </div>
+    );
+  }
+}
 export default App;
