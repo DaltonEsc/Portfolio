d8db5cbab4137a2e2c0c4dc8a1af68c1473be5ae
Dalton <eschberger.dalton@gmail.com>
2019-11-25 15:47:31 -0600

    Added dynamic loading components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Nav} from 'react-bootstrap'
-import Main from "./components/main";
-import './CSS/App.css';
+
+import './components/CSS/App.css';
+import Landing from "./components/landing";
+import Resume from "./components/resume";
+import Contact from "./components/contact";
+import Projects from "./components/projects";
+import Container from "react-bootstrap/Container";
+
 class App extends Component{
+    state={
+        landing:true,
+        resume: false,
+        projects:false,
+        contactMe:false
+    };
   render(){
     return (
         <div id='pageWrapper'>
-          <header>
-            <Navbar id='nav' expand="lg" >
-              <Navbar.Brand id='Brand' href="/">DALTON'S PORTFOLIO</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                  <Nav.Link className='links' href="https://github.com/DaltonEsc">GitHub</Nav.Link>
-                  <Nav.Link className='links' href="https://www.linkedin.com/in/dalton-e-b6368aa5/">LinkedIn</Nav.Link>
-                  <Nav.Link className='links' href="/resume">Resume</Nav.Link>
-                  <Nav.Link className='links' href="/projects">Projects</Nav.Link>
-                  <Nav.Link className='links' href="/contact">ContactMe</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Navbar>
-          </header>
-          <body>
-          <Main/>
-          </body>
+            <div id='contentWrapper'>
+                   <Navbar id='nav' expand="lg" >
+                        <Navbar.Brand onClick={()=>{
+                            this.setState({resume:false,landing:true,projects:false,contactMe:false});
+                        }} id='Brand' href="/">DALTON'S PORTFOLIO</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ml-auto">
+                                <Nav.Link href='' onClick={()=>{
+                                    this.setState({resume:false,landing:true,projects:false,contactMe:false});
+                                }}>Home
+                                </Nav.Link>
+                                <Nav.Link className='links' target="_blank" href="https://github.com/DaltonEsc">GitHub</Nav.Link>
+                                <Nav.Link className='links' target="_blank" href="https://www.linkedin.com/in/dalton-e-b6368aa5/">LinkedIn</Nav.Link>
+                                <Nav.Link href='' onClick={()=>{
+                                    this.setState({resume:true,landing:false,projects:false,contactMe:false});
+                                }}>Resume
+                                </Nav.Link>
+                                <Nav.Link onClick={()=>{
+                                    this.setState({resume:false,landing:false,projects:true,contactMe:false});
+                                }}>Projects
+                                </Nav.Link>
+                                <Nav.Link onClick={()=>{
+                                    this.setState({resume:false,landing:false,projects:false,contactMe:true});
+                                }}>Contact
+                                </Nav.Link>
+
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                <div id='componentWrapper'>
+                    {this.state.landing ? <Landing className='component'/> :null}
+                    {this.state.resume ? <Resume className='component'/> :null}
+                    {this.state.projects ? <Projects className='component'/> :null}
+                    {this.state.contactMe ? <Contact className='component'/> :null}
+                </div>
+                <div id = 'footer'>
+                    <Navbar expand="lg" sticky="bottom" variant="light" bg="light">
+                        <Container>
+                            <Navbar.Brand href="#">TODO - Build Footer</Navbar.Brand>
+                        </Container>
+                    </Navbar>
+                </div>
+            </div>
         </div>
     );
   }
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,13 +1,37 @@
 import React, {Component} from "react";
+import "./CSS/landing.css"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-
-class Landing extends Component{
+class Landing extends Component {
     render() {
-        return(
-            <div>
-                <h1>Landing</h1>
+        return (
+            <div id='landingWrapper'>
+                <div id='image'>
+                    <img width={300} height={300} src="https://avatars2.githubusercontent.com/u/9615942?s=460&v=4"
+                         alt=''/>
+                </div>
+                <div id='about'>
+                    <h1>Software Developer</h1>
+
+                    <Container>
+                        <Row id='icons'>
+                            <Col><img width={100} height={100}
+                                      src="https://img.icons8.com/windows/512/000000/github.png" alt=''/></Col>
+                            <Col><img width={80} height={80} src="https://img.icons8.com/metro/208/000000/linkedin.png"
+                                      alt=''/></Col>
+                            <Col><img width={100} height={100}
+                                      src="https://img.icons8.com/windows/512/000000/github.png" alt=''/></Col>
+                            <Col><img width={80} height={80} src="https://img.icons8.com/metro/208/000000/linkedin.png"
+                                      alt=''/></Col>
+                        </Row>
+                    </Container>
+                </div>
             </div>
+
         )
     }
 }
+
 export default Landing;
